07186c54ef8d93ac59c72bf4ba03741eb76c3d56
Roman <rakazakov@mail.ru>
2024-08-25 01:43:29 +0300

    object personalMovieDB added

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,8 @@
 let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+}
