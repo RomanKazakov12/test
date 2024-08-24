e23f979fe5b01c191cdf018eaa3de7a95b6d5dd3
Roman <rakazakov@mail.ru>
2024-08-25 01:59:16 +0300

    added questions in obj Movies

--- JS/script.js
+++ JS/script.js
@@ -6,3 +6,7 @@ let personalMovieDB = {
     genres: [],
     privat: false,
 }
+let lastFilmName = prompt("Один из последних просмотренных фильмов?", "");
+let lastFilmRating = prompt("На сколько баллов вы его оцените?", "");
+personalMovieDB.movies[lastFilmName] = lastFilmRating; 
+console.log (personalMovieDB);
