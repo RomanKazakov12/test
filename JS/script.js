657b078c408204167035fa5901f5ff4c09e46987
Roman <rakazakov@mail.ru>
2024-08-25 02:09:14 +0300

    personalMovieDB updated

--- JS/script.js
+++ JS/script.js
@@ -8,5 +8,8 @@ let personalMovieDB = {
 }
 let lastFilmName = prompt("Один из последних просмотренных фильмов?", "");
 let lastFilmRating = prompt("На сколько баллов вы его оцените?", "");
+let lastFilmNameB = prompt("Один из последних просмотренных фильмов?", "");
+let lastFilmRatingB = prompt("На сколько баллов вы его оцените?", "");
 personalMovieDB.movies[lastFilmName] = lastFilmRating; 
+personalMovieDB.movies[lastFilmNameB] = lastFilmRatingB; 
 console.log (personalMovieDB);
