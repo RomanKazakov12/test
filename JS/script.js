66edfdd321db30e7b493fe1e7216c87b87fe62a1
Roman <rakazakov@mail.ru>
2024-08-31 03:38:28 +0300

    cycles added

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 let personalMovieDB = {
     count: numberOfFilms,
@@ -6,10 +8,20 @@ let personalMovieDB = {
     genres: [],
     privat: false,
 }
-let lastFilmName = prompt("Один из последних просмотренных фильмов?", "");
-let lastFilmRating = prompt("На сколько баллов вы его оцените?", "");
-let lastFilmNameB = prompt("Один из последних просмотренных фильмов?", "");
-let lastFilmRatingB = prompt("На сколько баллов вы его оцените?", "");
-personalMovieDB.movies[lastFilmName] = lastFilmRating; 
-personalMovieDB.movies[lastFilmNameB] = lastFilmRatingB; 
+for (let i = 0; i < 2; i++) {
+    let lastFilmName = prompt("Один из последних просмотренных фильмов?", "");
+    let lastFilmRating = prompt("На сколько баллов вы его оцените?", "");
+    if (lastFilmName != "" && lastFilmName != null && lastFilmName.length < 50 && lastFilmRating != null && lastFilmRating != ""){
+        personalMovieDB.movies[lastFilmName] = lastFilmRating;
+        console.log ("Done")
+    } else {console.log ("Error");
+        i--;
+    }
+}
+
 console.log (personalMovieDB);
+if (personalMovieDB.count < 10) {
+    console.log ("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count < 30) {
+    console.log ("Вы классический зритель");
+} else {console.log ("Вы киноман")}
